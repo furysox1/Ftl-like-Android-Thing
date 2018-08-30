16d2aeff5cf991888d77089d2e7bf2f88c50c290
Elias <31489249+IzzetStaticaster@users.noreply.github.com>
2018-08-30 10:01:43 -0700

    Add files via upload

---
+++ SlowerThanLight/app/src/main/java/com/example/emedley9519/slowerthanlight/SlowerThanLightMainMenu.java
@@ -0,0 +1,13 @@
+package com.example.emedley9519.slowerthanlight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SlowerThanLightMainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slower_than_light_main_menu);
+    }
+}
